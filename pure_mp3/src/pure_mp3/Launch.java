723229a64b625c25e3edde73ea860341919216bd
s4ke <braun5@gmx.de>
2011-04-21 15:25:38 +0200

    Commented and chagend Launch.java

--- pure_mp3/src/pure_mp3/Launch.java
+++ pure_mp3/src/pure_mp3/Launch.java
@@ -1,8 +1,5 @@
 package pure_mp3;
 
-import java.awt.Window;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -15,17 +12,19 @@ import javax.sound.sampled.Mixer;
 import javax.sound.sampled.SourceDataLine;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.plaf.metal.MetalLookAndFeel;
 
 public class Launch 
 {
 	private static GUI gui;
-	public static void main (String args[])
+	
+	//Let's throw any Exceptions that happen, normally no Exception will ever be thrown.
+	public static void main (String args[]) throws Exception
     {
     	boolean support = false;
 		System.out.println("Available Mixers:");
+		//Let's look, if there are Mixers available
 		Mixer.Info[]	aInfos = AudioSystem.getMixerInfo();
 		for (int i = 0; i < aInfos.length; i++)			
 		{
@@ -38,6 +37,7 @@ public class Launch
 			}
 			else
 			{
+				//Oh a Mixer that doesn't like SourceDataLines. Tell that the cli
 				System.out.println("Not supported: " + aInfos[i].getName());
 			}
 		}
@@ -46,122 +46,100 @@ public class Launch
 			System.out.println("[No mixers available]");
 			System.exit(-1);
 		}
-		if(support)
+		//Not supported?
+		if(!support)
 		{
-			try 
-    	 	{
-				Object choices[] = {"Metal","Nimbus"};
-    	 		System.out.println(System.getProperty("os.name"));
-    	 		try
-    	 		{
-    	 			BufferedReader buffread = new BufferedReader(new FileReader(new File("puremp3","config.txt")));
-    	 			String laf = buffread.readLine();
-    	 			if(laf.equals("Metal"))
-    	 			{
-    	 				UIManager.setLookAndFeel(new MetalLookAndFeel());
-    	 			}
-    	 			else if(laf.equals("Nimbus"))
-    	 			{
-    	 				try 
-		        		{
-		        		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) 
-		        		    {
-		        		        if ("Nimbus".equals(info.getName())) 
-		        		        {
-		        		            UIManager.setLookAndFeel(info.getClassName());
-		        		            break;
-		        		        }
-		        		    }
-		        		}
-		        		catch(Exception ex)
-		        		{
-		        			JOptionPane.showMessageDialog(null,"Metal has been chosen, because your System doesn't support Nimbus.\nPlease update your config.txt");
-		        			UIManager.setLookAndFeel(new MetalLookAndFeel());
-		        		}
-    	 			}
-    	 			else
-    	 			{
-    	 				throw new Exception();
-    	 			}
-    	 		}
-    	 		catch(Exception e)
-    	 		{
-    	 			PrintWriter writer = null;
-    	 			int answer = JOptionPane.showOptionDialog(
-    	 					null,
-    	 					"Do you want to use Metal or Nimbus?", 
-    	 					"Which Look and Feel do you want?" , 
-    	 					JOptionPane.DEFAULT_OPTION, 
-    	 					JOptionPane.QUESTION_MESSAGE, 
-    	 					null, 
-    	 					choices,
-    	 					choices[0]);
-    	 			try
-    	 			{
-    	 				new File("puremp3").mkdir();
-    	 				writer = new PrintWriter(new FileWriter(new File("puremp3","config.txt")));
-    	 			}
-    	 			catch(Exception ex)
-    	 			{
-    	 				System.out.println("Failed saving LookAndFeel!");
-    	 			}
-		        	if(answer == 0)
+			JOptionPane.showMessageDialog(null,"Sound playback not supported properly!");
+		}
+		// if everything has worked so far, start executing
+		else
+		{
+			//but first choose the LaF
+			File config = new File("puremp3","config.txt");
+			//The predefined Choices for the LaF
+			Object choices[] = {"Metal","Nimbus"};			
+	 		System.out.println(System.getProperty("os.name"));
+	 		if(config.exists())
+	 		{
+	 			boolean success = false;
+	 			//read in the config file and see what it says
+	 			BufferedReader buffread = new BufferedReader(new FileReader(config));
+	 			//Read in the first line
+	 			String laf = buffread.readLine();
+	 			//if Metal is he LaF
+	 			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) 
+    		    {
+    		        if (laf.equals(info.getName())) 
+    		        {
+    		        	//set the LaF
+    		            UIManager.setLookAndFeel(info.getClassName());
+    		            //Everything worked fine
+    		            success = true;
+    		            break;
+    		        }
+    		    }
+	        	if(!success)
+	            {
+	        		JOptionPane.showMessageDialog(null,"The Default LaF has been chosen, because your System doesn't support your choice.\nPlease update your config.txt");
+	        	}
+	 		}
+	 		else
+	 		{
+	 			//first we need a PrintWriter variable.
+	 			PrintWriter writer = null;
+	 			//Let the user decide what he wants
+	 			int answer = JOptionPane.showOptionDialog(
+	 					null,
+	 					"Do you want to use Metal or Nimbus?", 
+	 					"Which Look and Feel do you want?" , 
+	 					JOptionPane.DEFAULT_OPTION, 
+	 					JOptionPane.QUESTION_MESSAGE, 
+	 					null, 
+	 					choices,
+	 					choices[0]);
+	 			try
+	 			{
+	 				//Create the directory for the config if it doesn't exist, yet
+	 				new File("puremp3").mkdir();
+	 				//initializing the PrintWriter
+	 				writer = new PrintWriter(new FileWriter(config));
+	 			}
+	 			catch(Exception ex)
+	 			{
+	 				System.out.println("Failed Creating the PrintWriter");
+	 			}
+	        	if(answer == 0 || answer == 1)
+	        	{
+	        		//Initialize the variable laf;
+	        		String laf = "";
+	        		if(answer == 0)
 		        	{
-		    	        UIManager.setLookAndFeel(new MetalLookAndFeel());
-		    	        if(writer != null)
-		    	        {
-		    	        	writer.println("Metal");
-		    	        	writer.flush();
-		    	        	writer.close();
-		    	        }
-		        	}
-		        	else if(answer == 1)
-		        	{
-		        		try 
-		        		{
-		        		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) 
-		        		    {
-		        		        if ("Nimbus".equals(info.getName())) 
-		        		        {
-		        		            UIManager.setLookAndFeel(info.getClassName());
-		        		            break;
-		        		        }
-		        		    }
-		        		}
-		        		catch(Exception ex)
-		        		{
-		        			JOptionPane.showMessageDialog(null,"Swing has been chosen, because your System doesn't support Nimbus.");
-		        			UIManager.setLookAndFeel(new MetalLookAndFeel());
-			    	        if(writer != null)
-			    	        {
-			    	        	writer.println("Metal");
-			    	        	writer.flush();
-			    	        	writer.close();
-			    	        }
-		        		}
-		        		if(writer != null)
-		    	        {
-		    	        	writer.println("Nimbus");
-		    	        	writer.flush();
-		    	        	writer.close();
-		    	        }
+		        		laf = "Metal";
 		        	}
 		        	else
 		        	{
-		        		System.exit(-1);
+		        		laf = "Nimbus";
 		        	}
-    	 		}    	 		
+		        	for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) 
+	    		    {
+	    		        if (laf.equals(info.getName())) 
+	    		        {
+	    		        	//Set the LaF according to the User
+	    		            UIManager.setLookAndFeel(info.getClassName());
+	    		            //And write it into the config
+	    		            writer.println("Metal");
+		    	        	writer.flush();
+		    	        	writer.close();
+	    		            break;
+	    		        }
+	    		    }
+	        	}
+	        	else
+	        	{
+	        		JOptionPane.showMessageDialog(null,"The Default LaF has been chosen, because your System doesn't support your choice.\nPlease update your config.txt");
+	        	}
     	    } 
-    	    catch (UnsupportedLookAndFeelException e)
-    	    {
-    	       System.out.println("Something is wrong with your Look and Feel!!!");
-    	    }
-			gui = new GUI();
-		}
-		else
-		{
-			System.out.println("Sound playback not supported properly!");
-			System.exit(-1);
+	 		gui = new GUI();
 		}
 	}
 
