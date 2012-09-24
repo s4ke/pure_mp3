2256182771b04e1bbfc2c0fc0f625e2ecde05c17
Martin Braun <martinbraun123@aol.com>
2012-09-25 01:02:38 +0200

    adaption to HotSound changes

--- pure_mp3/src/de/hotware/puremp3/console/ICommand.java
+++ pure_mp3/src/de/hotware/puremp3/console/ICommand.java
@@ -1,5 +1,24 @@
+/**
+ * File ICommand.java
+ * ---------------------------------------------------------
+ *
+ * Copyright (C) 2012 Martin Braun (martinbraun123@aol.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * - The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * - The origin of the software must not be misrepresented.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * TL;DR: As long as you clearly give me credit for this Software, you are free to use as you like, even in commercial software, but don't blame me
+ *   if it breaks something.
+ */
 package de.hotware.puremp3.console;
-
 
 public interface ICommand {
 	
--- pure_mp3/src/de/hotware/puremp3/console/PlayerConsole.java
+++ pure_mp3/src/de/hotware/puremp3/console/PlayerConsole.java
@@ -1,3 +1,23 @@
+/**
+ * File PlayerConsole.java
+ * ---------------------------------------------------------
+ *
+ * Copyright (C) 2012 Martin Braun (martinbraun123@aol.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * - The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * - The origin of the software must not be misrepresented.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * TL;DR: As long as you clearly give me credit for this Software, you are free to use as you like, even in commercial software, but don't blame me
+ *   if it breaks something.
+ */
 package de.hotware.puremp3.console;
 
 import java.io.IOException;
@@ -12,6 +32,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.regex.Pattern;
 
+import javax.sound.sampled.DataLine;
 import javax.sound.sampled.FloatControl;
 
 import de.hotware.hotsound.audio.player.BaseSong;
@@ -243,23 +264,28 @@ public class PlayerConsole implements Runnable {
 			@Override
 			public void execute(String... pArgs) throws UsageException {
 				int length = pArgs.length;
-				FloatControl floatControl = (FloatControl) this.mConsole.mMusicPlayer
-						.getControl(FloatControl.Type.MASTER_GAIN);
-				if(length == 1) {
-					this.mConsole.mPrintStream.println("Volume: " +
-							floatControl.getValue() + ", Min: " +
-							floatControl.getMinimum() + ", Max: " +
-							floatControl.getMaximum());
-				} else if(length == 2) {
-					float min = floatControl.getMinimum();
-					float max = floatControl.getMaximum();
-					float val = Integer.parseInt(pArgs[1]);
-					if(val < min) {
-						val = min;
-					} else if(val > max) {
-						val = max;
+				DataLine dataLine = this.mConsole.mMusicPlayer.getDataLine();
+				if(dataLine.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+					FloatControl floatControl = (FloatControl) dataLine
+							.getControl(FloatControl.Type.MASTER_GAIN);
+					if(length == 1) {
+						this.mConsole.mPrintStream.println("Volume: " +
+								floatControl.getValue() + ", Min: " +
+								floatControl.getMinimum() + ", Max: " +
+								floatControl.getMaximum());
+					} else if(length == 2) {
+						float min = floatControl.getMinimum();
+						float max = floatControl.getMaximum();
+						float val = Integer.parseInt(pArgs[1]);
+						if(val < min) {
+							val = min;
+						} else if(val > max) {
+							val = max;
+						}
+						floatControl.setValue(val);
 					}
-					floatControl.setValue(val);
+				} else {
+					this.mConsole.mPrintStream.println("Volume changes not supported on your platform!");
 				}
 			}
 
